7c3035b0c8655e2123a4c328594fada31018b70d
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-08-27 11:56:03 +0300

    commit tree 070856ec5a72eb35a047b0873fb6c9361877b43c

--- db/migrate/006_add_catchphrase_to_characters.rb
+++ db/migrate/006_add_catchphrase_to_characters.rb
@@ -1,7 +1,7 @@
 class AddCatchphraseToCharacters < ActiveRecord::Migration[6.1]
-  def change
-    # Your code here
-    add_column :characters, :catchphrase, :string  
+    def change
+      add_column :characters, :catchphrase, :string  
+    end
   end
-  end
-end
+  
+  
